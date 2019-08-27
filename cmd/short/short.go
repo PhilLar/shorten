3255bc286d41d37aa211ceef917ee05e17490ee1
PhilLar <phillarov@gmail.com>
2019-08-27 14:29:14 +0300

    changed os.Args for flag package

--- cmd/short/short.go
+++ cmd/short/short.go
@@ -3,54 +3,61 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
 )
 
 var api string = "https://cleanuri.com/api/v1/shorten"
+
+var flagRelink *bool = flag.Bool("relink", false, "a bool")
 
 type ApiAnswer struct {
 	Result_url string
 	Error      string
 }
 
-func ShortenUrl(args []string) (string, error) {
-	if len(args) == 2 {
-		url_link := args[1]
-		resp, err := http.PostForm(api, url.Values{
-			"url": {url_link},
-		})
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		answer := &ApiAnswer{}
-		err = json.Unmarshal(body, answer)
-		if err != nil {
-			log.Fatalln(err)
-		} else {
-			if answer.Error != "" {
-				return "", errors.New(answer.Error)
-			} else {
-				return answer.Result_url, nil
-			}
-		}
-		return string(body), nil
+func ShortenUrl() (string, error) {
+	flag.Parse()
+	if !*flagRelink {
+		return  CleanUrl(flag.Arg(0))
+	} else {
+		return "", errors.New("underdone")
 	}
-	return "", errors.New("Threre're must be 1 arg - a url!")
 }
 
+func CleanUrl(url_link string) (string, error) {
+	resp, err := http.PostForm(api, url.Values{
+		"url": {url_link},
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	answer := &ApiAnswer{}
+	err = json.Unmarshal(body, answer)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if answer.Error != "" {
+		return "", errors.New(answer.Error)
+	} else {
+		return answer.Result_url, nil
+	}
+}
+
+//func CleanUrl(url_link string) (string, error) {
+
 func main() {
-	result, err := ShortenUrl(os.Args)
+	result, err := ShortenUrl()
 	if err != nil {
 		log.Fatal(err)
 	}
