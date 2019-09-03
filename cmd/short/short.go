9a54b683ffa63266fa6c034dac0df65b912b1fa0
PhilLar <phillarov@gmail.com>
2019-09-03 15:44:32 +0300

    new functionality due to changes in deps

--- cmd/short/short.go
+++ cmd/short/short.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"log"
 	"github.com/PhilLar/webshorten/short"
+	"log"
+	"sync"
 )
 
+var flagRelink = flag.Bool("relink", false, "use rel.ink service to shorten URL")
+
+// runInParallel is a Wrapper for CleanUrl() and Relink()
+func runInParallel(a func(urlLink string) (string, error), args []string) map[string]error {
+	shorts := make(map[string]error)
+	var mutex = &sync.Mutex{}
+	var wg sync.WaitGroup
+	wg.Add(len(args))
+	for _, arg := range args {
+		go func(arg string) {
+			defer wg.Done()
+			short, err := a(arg)
+			mutex.Lock()
+			shorts[short] = err
+			mutex.Unlock()
+		}(arg)
+	}
+	wg.Wait()
+	return shorts
+}
+
+func shortenUrls() map[string]error {
+	flag.Parse()
+	if !*flagRelink {
+		return runInParallel(short.CleanURL, flag.Args())
+	}
+	return runInParallel(short.Relink, flag.Args())
+}
+
 func main() {
-	resultMap := short.ShortenUrls()
+	resultMap := shortenUrls()
 	for short, err := range resultMap {
 		if err == nil {
 			fmt.Println(short)
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/PhilLar/shorten
 
 go 1.12
 
-require github.com/PhilLar/webshorten v0.0.0-20190830105303-9a48c6a2de55
+require github.com/PhilLar/webshorten v0.0.0-20190903123917-7fb60dc87638
